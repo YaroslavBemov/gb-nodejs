724d1657b66c519bd509fbbce244a5a9d6fe6ce0
Yaroslav <yaroslav.bemov@gmail.com>
2021-10-29 20:31:26 +0300

    replace example

--- hw-3/task-1/src/index.js
+++ hw-3/task-1/src/index.js
@@ -4,59 +4,32 @@
 
 const fs = require('fs')
 const path = require('path')
-const {PassThrough} = require('stream')
+const { PassThrough } = require('stream')
+const readline = require('readline')
 
-// const {getArgs} = require('./utils')
+const { getArgs } = require('./utils')
 
-// const args = getArgs()
+const args = getArgs()
 
-// for (let i = 0; i < args.length; i++) {
-//   const writeStream = fs.createWriteStream(path.join(__dirname, `%${args[i]}%_requests.log`), {
-//     flags: 'a',
-//     encoding: 'utf8'
-//   })
-//
-//   // writeStream.write(log1)
-//
-//   // writeStream.end(() => console.log(`File writing finished`))
-// }
+for (let i = 0; i < args.length; i++) {
+  const arg = args[i]
+  const re = new RegExp(`^${arg}`)
 
-const readStream = fs.createReadStream(path.join(__dirname, 'access2.log'), 'utf-8')
-const writeStream = fs.createWriteStream(path.join(__dirname, '%ip-адрес%_requests.log'))
-const tunnel = new PassThrough()
+  const readStream = fs.createReadStream(path.join(__dirname, 'access2.log'), 'utf-8')
+  const writeStream = fs.createWriteStream(path.join(__dirname, `%${arg}%_requests.log`))
 
-tunnel.on('data', (chunk) => {
-  console.log(chunk.toString())
-})
+  const rl = readline.createInterface({
+    input: readStream,
+    output: writeStream
+  })
 
-readStream.pipe(tunnel).pipe(writeStream)
+  rl.on('line', function (line) {
+    if (re.test(line)) {
+      this.output.write(`${line}\n`)
+    }
+  })
 
-
-readStream.on('open', () => {
-  console.log('Read stream start...')
-})
-
-// readStream.on('data', (chunk) => {
-//   console.log(chunk)
-// })
-
-readStream.on('close', () => {
-  console.log('Read stream close...')
-})
-
-
-// import stream from 'stream';
-//
-// const duplex = new stream.Duplex({
-//   write: (chunk, encoding, next) {
-//   // Do something with the chunk and then call next() to indicate
-//   // that the chunk has been processed. The write() fn will handle
-//   // data piped into this duplex stream. After the write() has
-//   // finished, the data will be processed by the read() below.
-//   next();
-// },
-// read: ( size ) {
-//   // Add new data to be read by streams piped from this duplex
-//   this.push( "some data" )
-// }
-// })
+  rl.on('close', function () {
+    console.log(`Created "${this.output.path}"`)
+  })
+}
