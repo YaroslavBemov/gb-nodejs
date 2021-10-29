e44858f64c731afd491c0e92251cdd2356ab0e3a
Yaroslav <yaroslav.bemov@gmail.com>
2021-10-29 20:55:52 +0300

    done

--- hw-3/task-1/src/index.js
+++ hw-3/task-1/src/index.js
@@ -1,11 +1,9 @@
-// Напишите программу, которая находит в этом файле все записи
-// с ip-адресами 89.123.1.41 и 34.48.240.111,
-// а также сохраняет их в отдельные файлы с названием “%ip-адрес%_requests.log”.
-
 const fs = require('fs')
 const path = require('path')
 const { PassThrough } = require('stream')
 const readline = require('readline')
+
+require('dotenv').config()
 
 const { getArgs } = require('./utils')
 
@@ -15,7 +13,7 @@ for (let i = 0; i < args.length; i++) {
   const arg = args[i]
   const re = new RegExp(`^${arg}`)
 
-  const readStream = fs.createReadStream(path.join(__dirname, 'access2.log'), 'utf-8')
+  const readStream = fs.createReadStream(path.join(__dirname, process.env.TARGET_FILE), 'utf-8')
   const writeStream = fs.createWriteStream(path.join(__dirname, `%${arg}%_requests.log`))
 
   const rl = readline.createInterface({
